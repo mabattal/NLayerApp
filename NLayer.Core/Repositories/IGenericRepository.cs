8b79098a1ea9b7b596200c99be23883c1c2d454f
mabattal <mehmetalibattal@gmail.com>
2023-10-23 14:18:32 +0300

    IGenericRepository

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        // productRepository.where(x => x.id > 5).OrderBy.ToListAsync();
+        // veritabanından getirmeden where, orderby gibi sınırlayıcılar kullanabilmek için List yerine IQuarable kullandık
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
