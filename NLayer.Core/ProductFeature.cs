58292cb8bad43c632030a83907ba90b00d01516c
mabattal <mehmetalibattal@gmail.com>
2023-10-23 15:38:13 +0300

    Entity Configuration

--- NLayer.Core/ProductFeature.cs
+++ NLayer.Core/ProductFeature.cs
@@ -12,6 +12,7 @@ namespace NLayer.Core
         public string Color { get; set; }
         public decimal Height { get; set; }
         public decimal Width { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
     }
 }
