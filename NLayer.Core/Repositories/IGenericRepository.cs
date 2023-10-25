3e18ef1856fa9187d11ac9aef2eb25b3a89b8c21
mabattal <mehmetalibattal@gmail.com>
2023-10-25 13:56:11 +0300

    Temizlik

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -6,9 +6,6 @@ namespace NLayer.Core.Repositories
     {
         Task<T> GetByIdAsync(int id);
         IQueryable<T> GetAll();
-
-        // productRepository.where(x => x.id > 5).OrderBy.ToListAsync();
-        // veritabanından getirmeden where, orderby gibi sınırlayıcılar kullanabilmek için List yerine IQuarable kullandık
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
