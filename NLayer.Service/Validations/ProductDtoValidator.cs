f6f02ed6d1d5d6f8985fa3eaff86f5eab74c8495
mabattal <mehmetalibattal@gmail.com>
2023-10-27 15:28:25 +0300

    FluentValidation Library

---
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+
+namespace NLayer.Service.Validations
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is requared.").NotEmpty().WithMessage("{PropertyName} is requared.");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+        }
+    }
+}
