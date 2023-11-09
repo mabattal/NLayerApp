5cc2cea94d79dbcf11c2b4a423ccd06e81704305
mabattal <mehmetalibattal@gmail.com>
2023-11-09 15:41:07 +0300

    FluentValidation

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -10,7 +10,7 @@ namespace NLayer.Service.Validations
             RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is requared.").NotEmpty().WithMessage("{PropertyName} is requared.");
             RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
             RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
-            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} is requared.");
         }
     }
 }
