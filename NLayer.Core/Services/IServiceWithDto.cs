9d5ac9e53ca168ede968ca22751ad9ea36e08fc4
mabattal <mehmetalibattal@gmail.com>
2023-11-16 15:32:50 +0300

    IServiceWithDto interface

---
+++ NLayer.Core/Services/IServiceWithDto.cs
@@ -0,0 +1,23 @@
+﻿using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IServiceWithDto<Entity, Dto> where Entity : BaseEntity where Dto: class
+    {
+        Task<CustomResponseDto<Dto>> GetByIdAsync(int id);
+        Task<CustomResponseDto<IEnumerable<Dto>>> GetAllAsync();
+        Task<CustomResponseDto<IEnumerable<Dto>>> Where(Expression<Func<Entity, bool>> expression);
+        Task<CustomResponseDto<bool>> AnyAsync(Expression<Func<Entity, bool>> expression);
+        Task<CustomResponseDto<Dto>> AddAsync(Dto dto);
+        Task<CustomResponseDto<IEnumerable<Dto>>> AddRangeAsync(IEnumerable<Dto> dto);
+        Task<CustomResponseDto<Dto>> UpdateAsync(Dto dto);
+        Task<CustomResponseDto<Dto>> RemoveAsyn(int id);
+        Task<CustomResponseDto<Dto>> RemoveRangeAsync(IEnumerable<int> ids);
+    }
+}
